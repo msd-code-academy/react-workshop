22d64a3032b6b50b636bbbca3ef38fa090d17f66
Peter Hozak <peter.hozak@merck.com>
2018-11-07 17:01:45 +0100

    finish 01-props, extract Search and Result components using props

--- 00-props-state/search-example/src/App.js
+++ 00-props-state/search-example/src/App.js
@@ -1,6 +1,30 @@
 import React, {Component} from 'react';
 import './App.css';
 
+const fakeRes1 = [{title: 'Result 1', body: 'Long Description'}]
+const fakeRes2 = [{title: 'Result 1', body: 'Description D'}, {title: 'Result 2', body: 'Description E'}]
+
+const Result = ({result}) => {
+  const {title, body} = result
+  return (
+    <div>
+      <h2>{title}</h2> {body}
+    </div>
+  )
+}
+
+const Search = ({query, results}) => (
+  <div>
+    <div>
+      Search: <input value={query}/>
+      <button>Go</button>
+    </div>
+    <div>
+      Results for {query}: {results.map(result => <Result result={result} key={result.title}/>)}
+    </div>
+  </div>
+)
+
 class App extends Component {
   render() {
     return (
@@ -8,18 +32,11 @@ class App extends Component {
         <header className="App-header">
           logo, navigation, ...
         </header>
-        <div>
-          <div>
-            Search: <input value="abc"/>
-            <button>Go</button>
-          </div>
-          <div>
-            Results for abc:
-            <div>
-              <h2>Result 1</h2>
-              Long description </div>
-          </div>
-        </div>
+
+        <Search query={"abc"} results={fakeRes1}/>
+
+        <Search query={"def"} results={fakeRes2}/>
+
         <footer>
           MIT License
         </footer>
