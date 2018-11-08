76d381db8f44ec2a89ed96b22035552e70ef4bf5
Peter Hozak <peter.hozak@merck.com>
2018-11-08 11:58:10 +0100

    06-hooks, convert classes to Hooks

--- 00-props-state/search-example/src/App.js
+++ 00-props-state/search-example/src/App.js
@@ -1,30 +1,29 @@
-import React, {Component, PureComponent} from 'react';
-import {withRouter, RouterContext} from './router';
+import React, {useState, useEffect, useContext} from 'react';
 import './App.css';
 
 const fakeRes1 = [{title: 'Result 1', body: 'Long Description', more: 'abc'}]
 const fakeRes2 = [{title: 'Result 1', body: 'Description D', more: 'def'}, {title: 'Result 2', body: 'Description E'}]
 
-class Result extends Component {
-  handleClick = (e) => {
+export const RouterContext = React.createContext()
+const getHash = () => window.decodeURI(window.location.hash.substr(1))
+
+const Result = ({title, body, more}) => {
+  const {route, setRoute} = useContext(RouterContext)
+
+  const handleClick = (e) => {
     e.preventDefault()
-    const {more} = this.props
-    const {route, changeRoute} = this.context
-    changeRoute(`${route} ${more}`)
+    setRoute(`${route} ${more}`)
   }
-  render(){
-    const {title, body, more} = this.props
-    return (
-      <div>
-        <h1>{title}
-        </h1>
-        {body}
-        {more && <div><button onClick={this.handleClick}>See more</button></div>}
-      </div>
-    )
-  }
+
+  return (
+    <div>
+      <h1>{title}
+      </h1>
+      {body}
+      {more && <div><button onClick={handleClick}>See more</button></div>}
+    </div>
+  )
 }
-Result.contextType = RouterContext
 
 const Search = ({query, submittedQuery, results, onChange, onSubmit}) => (
   <div>
@@ -39,80 +38,50 @@ const Search = ({query, submittedQuery, results, onChange, onSubmit}) => (
     )}
   </div>
 )
+const App = () => {
+  const [route, setRoute] = useState(getHash())
+  const [query, setQuery] = useState('')
+  const [results, setResults] = useState([])
 
-class SearchLogic extends PureComponent {
-  state = {
-    query: this.props.route || '',
-    submittedQuery: '',
-    results: []
-  }
-  
-  componentDidMount() {
-    if (this.props.route) {
-      this.handleSubmit()
-    }
-  }
-  
-  componentWillReceiveProps(nextProps) {
-    if(this.props.route !== nextProps.route) {
-      this.setState({query: nextProps.route})
-      this.handleSubmit()
-    }
-  }
+  useEffect(() => {
+    setQuery(route)
+    window.location.hash = route
+    const timeout = setTimeout(() => setResults(Math.random() < 0.5 ? fakeRes1 : fakeRes2), 500)
+    return () => clearTimeout(timeout)
+  }, [route])
 
-  handleChange = (e) => {
-    this.setState({query: e.target.value})
-  }
+  useEffect(() => {
+    window.onhashchange = () => setRoute(getHash())
+    return () => window.onhashchange = undefined
+  }, [])
 
-  handleSubmit = (e) => {
-    e && e.preventDefault()
-    this.setState((state) => ({submittedQuery: state.query, results: []}))
-    // fake API call
-    setTimeout(() => {
-      this.setState({results: Math.random() < 0.5 ? fakeRes1 : fakeRes2})
-    }, 500)
+  const handleChange = (e) => {
+    setQuery(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setRoute(query)
   }
 
-  render() {
-    const {query, submittedQuery, results} = this.state
-    const {handleChange, handleSubmit} = this
-    return this.props.children({query, submittedQuery, results, handleChange, handleSubmit})
-  }
+  return (
+    <div className="App">
+      <RouterContext.Provider value={{route, setRoute}}>
+        <header className="App-header">
+          logo, navigation, ... {results.length ? `Showing ${results.length} results` : null}
+        </header>
+        <Search {...{query, results}}
+          submittedQuery={route}
+          onChange={handleChange}
+          onSubmit={handleSubmit}/>
+        <footer>
+          MIT License
+          {route &&
+           <div>More info about <a href={`https://google.com?q=${route}`}>{route}</a>.</div>
+          }
+        </footer>
+      </RouterContext.Provider>
+    </div>
+  );
 }
 
-class App extends PureComponent {
-  createHandlerSubmit = (newRoute) => (e) => {
-    const {changeRoute} = this.props // from withRouter
-    changeRoute(newRoute)
-  }
-  
-  render() {
-    const {route} = this.props
-    return (
-      <div className="App">
-        <SearchLogic route={route}>
-          {({query, submittedQuery, results, handleChange}) => (
-            <>
-              <header className="App-header">
-                logo, navigation, ... {results.length ? `Showing ${results.length} results` : null}
-              </header>
-              <Search {...{query, submittedQuery, results}}
-                onChange={handleChange}
-                onSubmit={this.createHandlerSubmit(query)}/>
-              <footer>
-                MIT License
-                {submittedQuery &&
-                  <div>More info about <a href={`https://google.com?q=${submittedQuery}`}>{submittedQuery}</a>.</div>
-                }
-              </footer>
-            </>
-          )}
-        </SearchLogic>
-      </div>
-    );
-  }
-}
-
-const AppWithRouter = withRouter(App)
-
-export default AppWithRouter;
+export default App;
