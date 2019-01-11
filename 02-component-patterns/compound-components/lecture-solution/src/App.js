5fff746319f7d81dba07d0d71393f3c2ce2c3582
PrincAm <ratbo90210@gmail.com>
2019-01-11 13:13:16 +0100

    add search bar and logo

--- 02-component-patterns/compound-components/lecture-solution/src/App.js
+++ 02-component-patterns/compound-components/lecture-solution/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './Navigation'
+import SearchBar from './SearchBar'
+import Logo from './Logo'
 
 class App extends Component {
   render() {
@@ -9,8 +11,11 @@ class App extends Component {
         <Navigation>
           <Navigation.Item to="/" label="Home" />
           <Navigation.Split />
+          <Logo />
+          <Navigation.Split />
           <Navigation.Item to="/messages" label="Messages" />
           <Navigation.Item to="/notifications" label="Notification" />
+          <SearchBar />
           <Navigation.Item to="/profile" label="Profile" />
         </Navigation>
       </div>
---
+++ 02-component-patterns/compound-components/lecture-solution/src/Logo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCrow} from '@fortawesome/free-solid-svg-icons'
+
+const Logo = () => <FontAwesomeIcon icon={faCrow} />
+
+export default Logo
