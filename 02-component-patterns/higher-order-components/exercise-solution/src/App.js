32b53d4f09866789c3a0f49547f968e5a412085e
PrincAm <ratbo90210@gmail.com>
2019-01-13 20:37:29 +0100

    first version of HOC exercise app implementation

---
+++ 02-component-patterns/higher-order-components/exercise-solution/src/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import hoistStatics from "hoist-non-react-statics";
+import './App.css';
+
+const getDisplayName = (Component) => {
+  return Component.displayName || Component.name || 'Component'
+}
+
+const withScreenDimensions = (WrappedComponent) => {
+  class ScreenDimensions extends Component {
+    state = {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
+
+    componentDidMount() {
+      window.addEventListener("resize", this.handleResize)
+    }
+
+    handleResize = () => {
+      this.setState({
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+    }
+
+    render() {
+      const {forwardRef, ...props} = this.props
+      return (
+        <WrappedComponent
+          ref={forwardRef}
+          {...props}
+          screenDimensions={this.state}
+        />
+      )
+    }
+  }
+
+  const displayName = getDisplayName(WrappedComponent)
+  ScreenDimensions.displayName = `withScreenDimensions(${displayName})`
+
+  const ForwardRef = React.forwardRef((props, ref) => (
+    <ScreenDimensions forwardRef={ref} {...props} />
+  ))
+  hoistStatics(ForwardRef, WrappedComponent)
+  return ForwardRef
+}
+
+class App extends Component {
+  static version = '2.0.0'
+  render() {
+    const {screenDimensions} = this.props
+    return (
+      <div className="App">
+        <h1>{screenDimensions.width}px by {screenDimensions.height}px</h1>
+      </div>
+    );
+  }
+}
+
+export default withScreenDimensions(App);
