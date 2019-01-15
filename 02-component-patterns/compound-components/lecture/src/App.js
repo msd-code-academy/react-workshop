a1f5f322c141129c94c99736b7f8551631f03819
PrincAm <ratbo90210@gmail.com>
2019-01-15 13:59:19 +0100

    compound components - add lecture example

--- 02-component-patterns/compound-components/lecture/src/App.js
+++ 02-component-patterns/compound-components/lecture/src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Navigation from './Navigation'
+import Logo from './Logo'
+
+// Compound components!
+const navItems = [
+  { to: "/", label: "Home" },
+  { to: "/messages", label: "Messages" },
+  { to: "/notifications", label: "Notifications" },
+  { to: "/profile", label: "Profile" }
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navigation items={navItems} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ 02-component-patterns/compound-components/lecture/src/Navigation.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+import cx from 'classnames'
+
+import './App.css'
+
+const NavigationItem = ({label, to, isActive}) => (
+  <li
+    className={cx({
+      'nav-active': isActive,
+    })}>
+    <a href={to}>{label}</a>
+  </li>
+)
+
+class Navigation extends React.Component {
+  static Item = NavigationItem
+  // Pretend this is dynamic and must be
+  // read from state!
+  state = {active: 'Home'}
+  render() {
+    const {children} = this.props
+    return (
+      <ul className="nav" role="navigation">
+        {children}
+      </ul>
+    )
+  }
+}
+
+export default Navigation
