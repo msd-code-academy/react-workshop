dd914363dd0879ecb622f6251801a3f7ebafa951
PrincAm <ratbo90210@gmail.com>
2019-01-17 13:35:01 +0100

    render props lecture - prepare url and data path to a comment

--- 02-component-patterns/exercises/render-props/lecture/src/App.js
+++ 02-component-patterns/exercises/render-props/lecture/src/App.js
@@ -3,6 +3,9 @@ import React, {Component} from 'react'
 import Fetch from './Fetch'
 import './App.css'
 
+// const URL = 'http://quotes.rest/qod.json?category=inspire'
+// data path data.contents.quotes[0].quote
+
 class App extends Component {
   render() {
     return (
