78452baac1f751c92812c85b9b067ce55fd90033
PrincAm <ratbo90210@gmail.com>
2019-09-05 16:34:38 +0200

    add commnet to context.js

--- 07-firebase/excersise/src/components/Firebase/context.js
+++ 07-firebase/excersise/src/components/Firebase/context.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const FirebaseContext = React.createContext(null)
 
+// Implement FirebaseContext.Consumer which then pass firebase instance to Component.
 export const withFirebase = (Component) => (props) => <div />
 
 export default FirebaseContext
